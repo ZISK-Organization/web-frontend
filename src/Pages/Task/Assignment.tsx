0887ac3a23b2066f8c0deed79e834d93d6710e02
StastnyJ <xstastnyj@gmail.com>
2021-10-31 03:49:52 +0100

    + mathajax package

--- src/Pages/Task/Assignment.tsx
+++ src/Pages/Task/Assignment.tsx
@@ -69,8 +69,10 @@ export default function Assignment({ taskId, modules, deadline, isTutorial }: IP
                             : ""
                         }
                     </head>
-                <body>
-                    ${content}
+                    <body>
+                      ${content}
+                      <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+                      <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
                     </body>
                 </html>`}
       />
