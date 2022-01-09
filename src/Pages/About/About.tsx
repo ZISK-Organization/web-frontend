5927edb4ed4d2d57a8a01dd9a869f1b075f099a1
Tomas Glozar <tglozar@gmail.com>
2022-01-09 11:37:14 +0100

    ~ fix typos in about

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
       <Typography variant="h4">O soutěži</Typography>
       <br />
       <Typography>
-        Zábavné Informatické Super Klání (ZISK) je internetová informatická soutěž určená primárně studentům středních škol a
+        Zábavné Informatické SuperKlání (ZISK) je internetová informatická soutěž určená primárně studentům středních škol a
         bakalářského studia vysokých škol. Řešit tedy může kdokoliv, ale úlohy jsou optimalizované pro tuto cílovou skupinu. Při
         tvorbě úloh se snažíme držet filosofie vyjádřené následujícími body...
       </Typography>
@@ -31,7 +31,7 @@ export default function About() {
       <Typography>
         Informatika je široký obor, ve kterém se na vyšší úrovni každý specializuje na něco jiného, a přijde nám škoda zaměřovat
         se čistě na algoritmizaci či úplné základy a opomíjet mnohé zajímavé disciplíny jako je kyberbezpečnost, procesory,
-        architektura operačních systémů, umělá inteligence, bioinformatika, či kvantové programování. Věříme, že je mnoho
+        architektura operačních systémů, umělá inteligence, bioinformatika či kvantové programování. Věříme, že je mnoho
         studentů s talentem pro různé oblasti této vědy, a chceme jim pomoci ho rozvíjet.
       </Typography>
       <br />
@@ -53,18 +53,18 @@ export default function About() {
       <br />
       <Typography>
         Soutěž se řeší ve čtyřech výplatních obdobích (sériích), přičemž každé výplatní období obsahuje čtyři úlohy čtyř
-        obtížností za které můžete dostat výplatu až deseti bodů. Tento ročník je první, tudíž o konkrétních kritériích pro
+        obtížností, za které můžete dostat výplatu až deseti bodů. Tento ročník je první, tudíž o konkrétních kritériích pro
         úspěšné řešitele či prioritě účasti na akcích bude rozhodnuto až podle zájmu a reálné míry obtížnosti úloh.
       </Typography>
       <br />
       <Typography variant="h4">Pravidla soutěže</Typography>
       <br />
-      <Typography>Je zakázáno používat software a hrdware ZISKu k jinému účelu, než který byl zamýšlen.</Typography>
+      <Typography>Je zakázáno používat software a hardware ZISKu k jinému účelu, než ke kterému byl zamýšlen.</Typography>
       <br />
       <Typography>Je zakázáno nahrávat škodlivý kód do odevzdávacích modulů.</Typography>
       <br />
       <Typography>
-        Je zakázáno zveřejňovat na našich stránkách či odevzdávat do modulů libovolný obsah v rozporu se zákony České Republiky.
+        Je zakázáno zveřejňovat na našich stránkách či odevzdávat do modulů libovolný obsah v rozporu se zákony České republiky.
       </Typography>
       <br />
       <Typography>Je zakázáno při řešení úloh spolupracovat s jinými osobami, zejména s ostatními řešiteli.</Typography>
